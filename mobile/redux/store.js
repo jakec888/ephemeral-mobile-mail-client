38104ebcc633989d1d4b53cc9b97fa3c9a8e9edb
jj888 <jaconjcondes@gmail.com>
2019-07-08 01:53:42 -0700

    redone with a blank template

--- mobile/redux/store.js
+++ mobile/redux/store.js
@@ -1,25 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-// import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
-// import { composeWithDevTools } from 'remote-redux-devtools';
 import { combineReducers } from 'redux';
 import rootReducer from './reducers/reducers';
-
-// const store = createStore(
-//   reducers,
-//   compose(
-//     applyMiddleware(ReduxThunk),
-//     devToolsEnhancer()
-//   )
-// );
-
-// const store = createStore(
-//   rootReducer,
-//   {},
-//   composeWithDevTools(applyMiddleware(ReduxThunk))
-// );
-
-// export default store;
 
 const composeEnhancers = compose(
   applyMiddleware(ReduxThunk),
