578b33cd6a95c2da01de4a20e50566fded8e0f6d
jj888 <jaconjcondes@gmail.com>
2019-07-08 19:26:50 -0700

    working drawer but wrong documentation implementation from react navigator

--- mobile/App.js
+++ mobile/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Container, Content, Picker, Button, Text } from 'native-base';
+// import Expo from 'expo';
+import { AppLoading } from 'expo';
 
-import HomeScreen from './Screens/HomeScreen';
-import store from './redux/store';
-
-export default function App() {
-  return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <HomeScreen />
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    </Provider>
-  );
+import HomeScreen from './src/HomeScreen/index.js';
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    };
+  }
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      Ionicons: require('native-base/Fonts/Ionicons.ttf')
+    });
+    await this.setState({ isReady: true });
+  }
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+    return <HomeScreen />;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   }
+// });
--- mobile/redux/reducers/reducers.js
+++
@@ -1,8 +0,0 @@
-// import { combineReducers } from 'redux';
-
-// import SampleReducer from './sample.reducer';
-import UserReducer from './user.reducers';
-
-export default {
-  User: UserReducer
-};
