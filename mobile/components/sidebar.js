3925af021c58131303695617f6124a8aca24a2a7
jj888 <jaconjcondes@gmail.com>
2019-07-12 12:58:41 -0700

    fixed position of nav drawer

--- mobile/components/sidebar.js
+++ mobile/components/sidebar.js
@@ -7,40 +7,34 @@ const CustomeSideBar = (props) => (
   <ScrollView>
     <SafeAreaView
       style={styles.container}
-      // forceInset={{ top: 'always', horizontal: 'never' }}
+      forceInset={{ top: 'always', horizontal: 'never' }}
     >
-      <Container>
-        <Image
-          source={{
-            uri:
-              'https://raw.githubusercontent.com/GeekyAnts/NativeBase-KitchenSink/master/assets/drawer-cover.png'
-          }}
-          style={{
-            height: 120,
-            width: '100%',
-            alignSelf: 'stretch',
-            position: 'absolute'
-          }}
-        />
-        <Image
-          square
-          style={{
-            height: 80,
-            width: 70,
-            position: 'absolute',
-            alignSelf: 'center',
-            top: 20
-          }}
-          source={{
-            uri:
-              'https://raw.githubusercontent.com/GeekyAnts/NativeBase-KitchenSink/master/assets/logo.png'
-          }}
-        />
-        {/* <DrawerItems {...props} /> */}
-      </Container>
-      <Container>
-        <DrawerItems {...props} />
-      </Container>
+      <Image
+        source={{
+          uri:
+            'https://raw.githubusercontent.com/GeekyAnts/NativeBase-KitchenSink/master/assets/drawer-cover.png'
+        }}
+        style={{
+          height: 120,
+          width: '100%',
+          alignSelf: 'stretch'
+        }}
+      />
+      <Image
+        square
+        style={{
+          height: 80,
+          width: 70,
+          position: 'absolute',
+          alignSelf: 'center',
+          top: 65
+        }}
+        source={{
+          uri:
+            'https://raw.githubusercontent.com/GeekyAnts/NativeBase-KitchenSink/master/assets/logo.png'
+        }}
+      />
+      <DrawerItems {...props} />
     </SafeAreaView>
   </ScrollView>
 );
