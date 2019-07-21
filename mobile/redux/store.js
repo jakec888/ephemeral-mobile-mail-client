e73502a67b1c736d9d0a44d9aeaa93750326d97f
jj888 <jaconjcondes@gmail.com>
2019-07-21 16:02:51 -0700

    devToolsEnhancer() does not work; fixed debuger displaying state

--- mobile/redux/store.js
+++ mobile/redux/store.js
@@ -3,12 +3,13 @@ import ReduxThunk from 'redux-thunk';
 import { combineReducers } from 'redux';
 import rootReducer from './reducers/reducers';
 
-import devToolsEnhancer from 'remote-redux-devtools';
+// import devToolsEnhancer from 'remote-redux-devtools';
+// Dev tools enhancer does not work
 
 const composeEnhancers = compose(
   applyMiddleware(ReduxThunk),
-  devToolsEnhancer()
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // devToolsEnhancer()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const store = createStore(combineReducers(rootReducer), composeEnhancers);
