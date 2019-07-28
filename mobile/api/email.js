91439a22edc820c7ba72cf86d12b3ebe01d26550
jj888 <jaconjcondes@gmail.com>
2019-07-27 22:33:32 -0700

    direct api string

--- mobile/api/email.js
+++ mobile/api/email.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
-import Config from '../Config';
+import Config from '../../Config';
 
 export default axios.create({
-  baseURL: Config.email.prod,
+  // baseURL: Config.email.prod,
+  baseURL: 'https://u5la3fxun4.execute-api.us-east-1.amazonaws.com/api',
   responseType: 'json'
 });
