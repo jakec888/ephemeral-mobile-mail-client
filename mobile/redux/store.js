6777bcc541ab2ab4aad4fb9b4854f2f1a8854da2
jj888 <jaconjcondes@gmail.com>
2019-07-30 16:35:28 -0700

    update redux thunk

--- mobile/redux/store.js
+++ mobile/redux/store.js
@@ -3,12 +3,8 @@ import ReduxThunk from 'redux-thunk';
 import { combineReducers } from 'redux';
 import rootReducer from './reducers/reducers';
 
-// import devToolsEnhancer from 'remote-redux-devtools';
-// Dev tools enhancer does not work
-
 const composeEnhancers = compose(
   applyMiddleware(ReduxThunk),
-  // devToolsEnhancer()
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
