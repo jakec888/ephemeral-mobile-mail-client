235ec22f1600a1def2853a9a554b5357ae080957
jj888 <jaconjcondes@gmail.com>
2019-08-07 23:52:08 -0700

    updated redux so it wont look for the devtools

--- mobile/redux/store.js
+++ mobile/redux/store.js
@@ -3,10 +3,15 @@ import ReduxThunk from 'redux-thunk';
 import { combineReducers } from 'redux';
 import rootReducer from './reducers/reducers';
 
-const composeEnhancers = compose(
-  applyMiddleware(ReduxThunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// Dev
+// redux will break if
+// const composeEnhancers = compose(
+//   applyMiddleware(ReduxThunk),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+// Production
+const composeEnhancers = compose(applyMiddleware(ReduxThunk));
 
 const store = createStore(combineReducers(rootReducer), composeEnhancers);
 
