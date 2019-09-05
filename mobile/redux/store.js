58901a450b8d9af16e37812d6cc4260e76f02a62
jj888 <jaconjcondes@gmail.com>
2019-09-05 11:49:02 -0700

    tests files and folder boilerplate added and redux-saga added as middleware to the redux store

--- mobile/redux/store.js
+++ mobile/redux/store.js
@@ -1,18 +1,34 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import { combineReducers } from 'redux';
-import rootReducer from './reducers/reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import ReduxThunk from "redux-thunk";
+import { combineReducers } from "redux";
+import rootReducer from "./reducers/reducers";
+import createSagaMiddleware from "redux-saga";
 
-// Dev
-// redux will break if
-// const composeEnhancers = compose(
-//   applyMiddleware(ReduxThunk),
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
+const ReduxSaga = createSagaMiddleware();
 
-// Production
-const composeEnhancers = compose(applyMiddleware(ReduxThunk));
+const middlewares = [ReduxThunk, ReduxSaga];
+
+const composeEnhancers = compose(applyMiddleware(...middlewares));
 
 const store = createStore(combineReducers(rootReducer), composeEnhancers);
 
 export default store;
+
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import ReduxThunk from 'redux-thunk';
+// import { combineReducers } from 'redux';
+// import rootReducer from './reducers/reducers';
+
+// // Dev
+// // redux will break if
+// // const composeEnhancers = compose(
+// //   applyMiddleware(ReduxThunk),
+// //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// // );
+
+// // Production
+// const composeEnhancers = compose(applyMiddleware(ReduxThunk));
+
+// const store = createStore(combineReducers(rootReducer), composeEnhancers);
+
+// export default store;
